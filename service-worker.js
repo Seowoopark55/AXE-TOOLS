b60de2a9c1d973699a4d799cd7f3ef27684e61a1
Seowoopark55 <dls18222@naver.com>
2026-06-17 11:18:02 +0900

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,49 @@
+const AXE_COOK_CACHE = 'axe-cook-v47-pwa-20260617';
+const AXE_COOK_ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icons/icon-192.png',
+  './icons/icon-512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(AXE_COOK_CACHE).then(cache => cache.addAll(AXE_COOK_ASSETS)).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.filter(key => key !== AXE_COOK_CACHE).map(key => caches.delete(key)))).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', event => {
+  const req = event.request;
+  const url = new URL(req.url);
+
+  if (url.hostname.includes('script.google.com') || url.hostname.includes('googleusercontent.com')) {
+    event.respondWith(fetch(req));
+    return;
+  }
+
+  if (req.mode === 'navigate') {
+    event.respondWith(
+      fetch(req).then(res => {
+        const copy = res.clone();
+        caches.open(AXE_COOK_CACHE).then(cache => cache.put('./index.html', copy));
+        return res;
+      }).catch(() => caches.match('./index.html'))
+    );
+    return;
+  }
+
+  event.respondWith(
+    caches.match(req).then(cached => cached || fetch(req).then(res => {
+      const copy = res.clone();
+      caches.open(AXE_COOK_CACHE).then(cache => cache.put(req, copy));
+      return res;
+    }))
+  );
+});
